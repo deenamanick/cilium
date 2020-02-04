458ad2300b7e41499f9e8729577a15d5c1817afe
Martynas Pumputis <m@lambda.lt>
2020-02-04 16:47:22 +0100

    test: Do not skip kube-proxy free tests when running w/o third host
    Instead, skip individual test cases which require the third host.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/k8sT/Services.go
+++ test/k8sT/Services.go
@@ -419,7 +419,7 @@ var _ = Describe("K8sServicesTest", func() {
 			})
 
 		SkipContextIf(
-			func() bool { return helpers.DoesNotRunOnNetNext() || helpers.DoesNotHaveHosts(3)() },
+			helpers.DoesNotRunOnNetNext,
 			"Tests NodePort BPF", func() {
 				// TODO(brb) Add with L7 policy test cases after GH#8971 has been fixed
 
@@ -483,7 +483,7 @@ var _ = Describe("K8sServicesTest", func() {
 					})
 				})
 
-				Context("Tests with MetalLB", func() {
+				SkipContextIf(helpers.DoesNotExistNodeWithoutCilium, "Tests with MetalLB", func() {
 					var (
 						metalLB string
 					)
@@ -508,7 +508,7 @@ var _ = Describe("K8sServicesTest", func() {
 					})
 				})
 
-				It("Tests with direct routing and DSR", func() {
+				SkipItIf(helpers.DoesNotExistNodeWithoutCilium, "Tests with direct routing and DSR", func() {
 					deleteCiliumDS(kubectl)
 					DeployCiliumOptionsAndDNS(kubectl, ciliumFilename, map[string]string{
 						"global.nodePort.enabled":     "true",
